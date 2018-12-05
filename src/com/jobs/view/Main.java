338e2a0d26eb21de473f08bfd28438d55da21034
Jonatan Arroyo <43411844+JonatanArroyoWork@users.noreply.github.com>
2018-12-05 12:40:49 +0100

    Load Files

---
+++ src/com/jobs/view/Main.java
@@ -0,0 +1,35 @@
+package com.jobs.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jobs.application.DTOStaffMember;
+import com.jobs.application.JobsController;
+import com.jobs.domain.AbsStaffMember;
+import com.jobs.domain.Employee;
+
+public class Main {
+
+	private static JobsController controller = new JobsController();
+
+	public static void main(String[] args) throws Exception {
+
+		controller.createBossEmployee("Pepe Boss", "Dirección molona", "666666666", 100.0);
+		controller.createEmployee("Pedro Employee", "Dirección molona 2", "665266666", 40.0);
+		controller.createEmployee("Laura Employee", "Dirección molona 3", "625266666", 45.0);
+		controller.createVolunteer("Juan Volunteer", "Dirección molona 4", "614266666", "NO Cobra");
+		controller.createManagerEmployee("Pedro Employee", "Dirección molona 2", "665226666", 80.0);
+
+		controller.payAllEmployeers();
+
+	
+		List<DTOStaffMember> allEmployees = controller.getAllEmployees();
+
+
+		for (DTOStaffMember d: allEmployees) {
+			
+			
+			System.out.println( "Empleado: " + d.getName() + " gets  Total   Paid:   " + d.getTotalPaid());
+		}
+	}
+}
